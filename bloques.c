cf91363adc28d9aa89b88cf07b85470289529092
Marcos Socías <105206059+MasoalM@users.noreply.github.com>
2025-02-15 20:37:59 +0100

    Comentarios escritos en todas las funciones

--- bloques.c
+++ bloques.c
@@ -1,7 +1,13 @@
 #include "bloques.h"
 
+// Variable global que almacena el descriptor de un dispositivo virtual
 static int descriptor = 0;
 
+// Nombre: bmount
+// Utilidad: Función que monta y abre el dispositivo virtual
+// Parámetros de entrada: camino (ruta donde crear o acceder al dispositivo virtual)
+// Salida: Devuelve -1 (FALLO) en caso de error y el descriptor de la opertura en caso de éxito
+// Dónde se utiliza: Main de mi_mkfs
 int bmount(const char *camino) {
     umask(000);
     descriptor = open(camino, O_CREAT | O_RDWR, 0666);
@@ -12,7 +18,11 @@ int bmount(const char *camino) {
     return descriptor;
 }
 
-
+// Nombre: bumount
+// Utilidad: Función que desmonta el dispositivo virtual llamando a la función close para liberar el descriptor del fichero
+// Parámetros de entrada: Ninguno
+// Salida: Devuelve -1 (FALLO) en caso de error y 0 (EXITO) en caso de cierre exitoso
+// Dónde se utiliza: Main de mi_mkfs
 int bumount() {
     int cierre = close(descriptor);
     if(cierre == -1) {
@@ -22,7 +32,11 @@ int bumount() {
     return EXITO;
 }
 
-
+// Nombre: bwrite
+// Utilidad: Función que escribe 1 bloque en el dispositivo virtual, en el bloque físico especificado por nbloque
+// Parámetros de entrada: nbloque (número de bloque) y buf (puntero al buffer de memoria)
+// Salida: Devuelve -1 (FALLO) en caso de error o la cantidad escrita (tamaño de un bloque, BLOCKSIZE) en caso de éxito
+// Dónde se utiliza: Main de mi_mkfs
 int bwrite(unsigned int nbloque, const void *buf) {
     off_t desplazamiento = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
     if(desplazamiento == -1) {
@@ -35,9 +49,14 @@ int bwrite(unsigned int nbloque, const void *buf) {
         perror(RED "Error al escribir un bloque");
         return FALLO;
     }
-    return BLOCKSIZE;
+    return bytes_escritos;
 }
 
+// Nombre: bread
+// Utilidad: Función que lee 1 bloque del dispositivo virtual, que se corresponde con el bloque físico especificado por nbloque
+// Parámetros de entrada: nbloque (número de bloque) y buf (puntero al buffer de memoria)
+// Salida: Devuelve -1 (FALLO) en caso de error o la cantidad leída (tamaño de un bloque, BLOCKSIZE) en caso de éxito
+// Dónde se utiliza: Main de mi_mkfs
 int bread(unsigned int nbloque, void *buf) {
     off_t desplazamiento = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
     if(desplazamiento == -1) {
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -2,10 +2,18 @@
 #include "bloques.c"
 #include <string.h>
 
+// Método main
+// Utilidad: monta el dispositivo virtual, escribe n bloques y desmonta el dispositivo virtual
+// Parámetros de entrada: argc (número de argumentos) y argv (array que contiene los argumentos)
+// Salida: Devuelve -1 (FALLO) en caso de error y 0 (EXITO) en caso de ejecución exitosa
 int main(int argc, char **argv) {
     unsigned char *buffer [BLOCKSIZE];
     memset(buffer, '\0', sizeof(buffer));
 
+    if(argc < 3) {
+        perror("Error, faltan argumentos: $ ./mi_mkfs <nombre_dispositivo> <nbloques>");
+        return FALLO;
+    } 
     if(strcmp(argv[0], "./mi_mkfs") != 0) {
         perror("Error de sintaxis");
         return FALLO;
